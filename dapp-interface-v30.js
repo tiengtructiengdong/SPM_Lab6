4178a31410e785d3b98366868ff84bd41b61f3f8
tiengtructiengdong <trucdong.txtv@gmail.com>
2023-08-25 12:46:44 +0700

    Update and rename dapp-interface-v29.js to dapp-interface-v30.js

--- dapp-interface-v30.js
+++ dapp-interface-v30.js
@@ -5148,7 +5148,7 @@
                   this,
                   Ai,
                   {
-                    all: { address: "12321" }, //(e = e || new Map()),
+                    all: e = e || new Map(),  // WHAT IS e
                     on: function (t, r) {
                       var n = e.get(t);
                       n ? n.push(r) : e.set(t, [r]);
